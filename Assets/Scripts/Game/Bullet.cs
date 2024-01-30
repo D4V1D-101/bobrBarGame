583fb4329b338cdf5baa12be90ed0ffcdb142e3e
Sufi <120452154+MarcalekM@users.noreply.github.com>
2024-01-30 10:40:42 +0100

    Fire

--- Assets/Scripts/Game/Bullet.cs
+++ Assets/Scripts/Game/Bullet.cs
@@ -5,25 +5,12 @@ using UnityEngine.InputSystem;
 
 public class Bullet : MonoBehaviour
 {
-    [SerializeField] private GameObject _bulletPrefrab;
-    [SerializeField] private float _bulletSpeed;
-    private bool _fireContinously;
-
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (_fireContinously) FireBullet();
-    }
-
-    private void FireBullet()
-    {
-        GameObject bullet = Instantiate(_bulletPrefrab, transform.position, transform.rotation);
-        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-
-        rb.velocity = _bulletSpeed * transform.up;
-    }
-
-    private void OnFire(InputValue inputValue)
-    {
-        _fireContinously = inputValue.isPressed;
+        if (collision.GetComponent<EnemyMove>())
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
     }
 }
