06a6196b194fe2925dbd6bc25d325a122d98dcd8
Sufi <matemarcalek@gmail.com>
2024-01-31 18:19:35 +0100

    EnemySounds

--- Assets/Scripts/Game/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Game/Enemy/EnemyAttack.cs
@@ -6,13 +6,20 @@ public class EnemyAttack : MonoBehaviour
 {
     [SerializeField]
     private float damageAmount;
-
+    [SerializeField] private AudioSource attackSound;
     private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.GetComponent<PlayerMove>())
         {
             var healthController = collision.gameObject.GetComponent<HealthController>();
+            attackSound.Play();
             healthController.takeDamage(damageAmount);
+            StartCoroutine(Wait());
         }
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(1f);
     }
 }
